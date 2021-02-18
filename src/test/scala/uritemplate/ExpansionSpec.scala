61d7ac5c5cdd637d5b8f1ea872836ffedab8f9be
slavik <s.novgorodtcev@newmotion.com>
2021-02-18 13:02:47 +0100

    Support Scala 2.12 and Scala 2.13

---
+++ src/test/scala/uritemplate/ExpansionSpec.scala
@@ -0,0 +1,33 @@
+package uritemplate
+
+import org.scalatest.prop.TableDrivenPropertyChecks.forAll
+import org.scalatest.prop.Tables.Table
+import org.scalatest.propspec.AnyPropSpec
+
+trait ExpansionSpec extends AnyPropSpec {
+
+  val variables: Map[String, Option[Variable]]
+
+  override def suiteName = name
+
+  def renderVariables = {
+    val keyLength = variables.keySet.map(_.length()).max
+    val strings = variables.map { case (key, value) =>
+      val padded: String = key + " " * (keyLength - key.length())
+      padded + " := " + value
+    }
+    strings.mkString("\t", "\n\t", "")
+  }
+
+  def name: String
+
+  def example(name: String)(examples: (String, String)*) {
+    property(name) {
+      val table = Table(("Expression", "Expansion"), examples: _*)
+      forAll(table) { (exp, ex) =>
+        assert((URITemplate(exp) expand variables) === ex)
+      }
+    }
+  }
+
+}
